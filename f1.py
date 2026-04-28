4d504919c953642d056b78d698313baf6064c016
Symeon Argiaditis <206509197+SymeonArgiaditis@users.noreply.github.com>
2026-04-28 21:50:23 +0300

    Add command-line argument support for displaying F1 standings

--- f1.py
+++ f1.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import argparse
 
 def get_standings() -> list: 
     response = requests.get("https://api.jolpi.ca/ergast/f1/2025/driverStandings/")
@@ -12,4 +13,14 @@ def print_standings(drivers) -> None:
     for driver in drivers:
         print(f"{driver["position"]}. {driver["Driver"]["givenName"]} {driver["Driver"]["familyName"]} - {driver["points"]} pts ({driver['Constructors'][0]['name']})")
 
-print_standings(get_standings())
+parser = argparse.ArgumentParser(description="print F1 standings")
+
+parser.add_argument("-s", "--standings", action="store_true",
+                    help="display 2025 driver standings")
+
+args = parser.parse_args()
+
+if args.standings:
+    print_standings(get_standings())
+else:
+    parser.print_help()
