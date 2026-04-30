c2eb4ece54d8d138cc19ad0cee3ca77e5a3e356c
Symeon Argiaditis <206509197+SymeonArgiaditis@users.noreply.github.com>
2026-04-30 23:37:12 +0300

    Refactor API request handling to use safe_request function for improved error management and DRY

--- f1.py
+++ f1.py
@@ -4,27 +4,29 @@ import sys
 
 ### API Calls ###
 
-def get_standings() -> list:
+def safe_request(url, timeout=(10,20)):
     try:
-        response = requests.get("https://api.jolpi.ca/ergast/f1/2026/driverstandings/", timeout=(10,20))
-        data = response.json()
-        return data["MRData"]["StandingsTable"]["StandingsLists"][0]["DriverStandings"]
+        response = requests.get(url, timeout=timeout)
+        return response
     except requests.exceptions.ConnectionError:
         raise ConnectionError("Error: could not connect to the F1 API. Check your internet connection.")
+
+def get_standings() -> list:
+    try:
+        data = safe_request("https://api.jolpi.ca/ergast/f1/2026/driverstandings/").json()
+        return data["MRData"]["StandingsTable"]["StandingsLists"][0]["DriverStandings"]
     except KeyError:
         raise KeyError("Error: unexpected response from the F1 API.")
 
 def get_race(choice) -> str:
     try:
-        response = requests.get("https://api.jolpi.ca/ergast/f1/2026/races/")
-        data = response.json()
+        data = safe_request("https://api.jolpi.ca/ergast/f1/2026/races/").json()
         races = data["MRData"]["RaceTable"]["Races"]
 
         if choice <= len(races) and choice >= 1:
             return races[choice-1]["raceName"]
         return "Race not found."
-    except requests.exceptions.ConnectionError:
-        raise ConnectionError("Error: could not connect to the F1 API. Check your internet connection.")
+    
     except KeyError:
         raise KeyError("Error: unexpected response from the F1 API.")
 
