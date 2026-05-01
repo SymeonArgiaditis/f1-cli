63f86d3ea0f48ad6c01e5a94f1bab96fa6bf6aae
Symeon Argiaditis <206509197+SymeonArgiaditis@users.noreply.github.com>
2026-05-01 19:25:01 +0300

    Remove unused imports from display.py

--- display.py
+++ display.py
@@ -1,5 +1,3 @@
-from api import get_standings, get_race
-
 def show_standings(drivers) -> None:
     for driver in drivers:
         print(f"{driver['position']}. {driver['Driver']['givenName']} {driver['Driver']['familyName']} - {driver['points']} pts ({driver['Constructors'][0]['name']})")
