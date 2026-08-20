a45df3bcdcff7bdceb53330b07b0f99e70dab4cf
Symeon Argiaditis <206509197+SymeonArgiaditis@users.noreply.github.com>
2026-08-20 17:23:40 +0300

    Add type hints to show_standings and show_race_info functions for improved clarity. Formated whitespace

--- display.py
+++ display.py
@@ -1,15 +1,25 @@
-def show_standings(drivers) -> None:
-    for driver in drivers:
-        print(f"{driver['position']}. {driver['Driver']['givenName']} {driver['Driver']['familyName']} - {driver['points']} pts ({driver['Constructors'][0]['name']})")
+from typing import Any
 
-def show_race_info(race) -> None:
-    name = race["raceName"]
-    circuit = race["Circuit"]
-    loc = circuit["Location"]
+def show_standings(drivers: list[dict[str, Any]]) -> None:
+    for driver in drivers:
+        print(
+            f"{driver['position']}. "
+            f"{driver['Driver']['givenName']} "
+            f"{driver['Driver']['familyName']} - "
+            f"{driver['points']} pts "
+            f"({driver['Constructors'][0]['name']})"
+        )
+
+
+def show_race_info(race: dict[str, Any]) -> None:
+    name: str = race["raceName"]
+    circuit: dict[str, Any] = race["Circuit"]
+    loc: dict[str, Any] = circuit["Location"]
     
-    print(f"====== {name} ======\n"
-          f"Location: {circuit['circuitName']} - {loc['locality']}, {loc['country']}\n"
-          f"Date: {race['date']}\n"
-          f"Wiki: {circuit['url']}\n"
-          )
-    
+    print(
+        f"====== {name} ======\n"
+        f"Location: {circuit['circuitName']} - "
+        f"{loc['locality']}, {loc['country']}\n"
+        f"Date: {race['date']}\n"
+        f"Wiki: {circuit['url']}\n"
+    )
