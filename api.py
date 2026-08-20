d39aaddd0de5532b95b8508e464a8892758462ce
Symeon Argiaditis <206509197+SymeonArgiaditis@users.noreply.github.com>
2026-08-20 17:24:18 +0300

    Fix formatting by adding blank lines for improved readability in api.py

--- api.py
+++ api.py
@@ -14,6 +14,7 @@ def safe_request(
             "Check your internet connection."
         )
 
+
 def get_standings() -> list[dict[str, Any]]:
     try:
         data: dict[str, Any] = safe_request(
@@ -26,6 +27,7 @@ def get_standings() -> list[dict[str, Any]]:
     
     except KeyError:
         raise KeyError("Error: unexpected response from the F1 API.")
+
 
 def get_race(choice: int) -> dict[str, Any]:
     try:
